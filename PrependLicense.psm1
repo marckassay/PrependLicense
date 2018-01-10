5d3b8b2d6183d6af00ddf986994826e6b9468133
Marc Kassay <marckassay@gmail.com>
2018-01-09 19:36:32 -0500

    Lots of piping changes.  This commit breaks build.

--- PrependLicense.psm1
+++ PrependLicense.psm1
@@ -242,12 +242,13 @@ function Start-PrependProcess {
         if ($IsContainer -eq $True) {
             Get-ChildItem -Path $Path -Recurse | ForEach-Object -Process {
                 if ($_.PSIsContainer -eq $False) {
-                    Add-PrependContent -Path $_.FullName -Value $Header -Include $Include -WhatIf:$WhatIf.IsPresent
+                    Open-File -FilePath $_.FullName | Write-File -Header $Header -Include $Include -WhatIf:$WhatIf.IsPresent | Close-File
                 }
             }
         }
         else {
-            Add-PrependContent -Path $Path -Value $Header -Include $Include -WhatIf:$WhatIf.IsPresent 
+            # Add-PrependContent -Path $Path -Value $Header -Include $Include -WhatIf:$WhatIf.IsPresent 
+            Open-File | Write-File | Close-File
         }
 
         Format-SummaryTable -WhatIf:$WhatIf.IsPresent
@@ -268,89 +269,23 @@ function Start-PrependProcess {
     }
 }
 
-function Add-PrependContent {
-    [CmdletBinding()]
-    Param
-    (
-        [Parameter(Mandatory = $True)]
-        [ValidateNotNullOrEmpty()]
-        [string]$Path,
-
-        [Parameter(Mandatory = $True)]
-        [string]$Value,
-
-        [Parameter(Mandatory = $False)]
-        [AllowEmptyString()]
-        [AllowNull()]
-        [string]$Include,
-
-        [switch]$WhatIf
-    )
-
-    $FileContents = Get-Item $Path -OutVariable File | Get-Content -Raw
-    if ($File.Extension -eq ".html") {
-        $HTMLDirective = $FileContents | Select-String -Pattern '.*DOCTYPE.*'
-        if ($HTMLDirective.Matches.Success) {
-            $FileContents = $FileContents.Replace($HTMLDirective.Matches.Value, "").Trim()
-        }
-    }
-    # if Include is defined and it contains the current Extension...
-    if ($Include -and $Include.Split(',').Contains('*' + $File.Extension) -eq $True) {
-        $HeaderPrependedToValue = @"
-${Value}
-${FileContents}
-"@
-    }
-    elseif (!$Include) {
-        $Brackets = Get-FileTypeBrackets -FileExtension $File.Extension
-
-        if ($Brackets) {
-            $HeaderPrependedToValue = @"
-$($Brackets.Opening.Trim())
-${Value}
-$($Brackets.Closing.Trim())
-${FileContents}
-"@
-        }
-    }
-    
-    if ($HeaderPrependedToValue) {
-        if($HTMLDirective.Matches.Success) {
-            $HeaderPrependedToValue = $HeaderPrependedToValue.Insert(0, $HTMLDirective.Matches.Value + "`r`n")
-        }
-        Write-ContentsToFile -WhatIf:$WhatIf.IsPresent
-
-        Set-SummaryTable -FileExtension $File.Extension -Modified $True
-    }
-    else {
-        Set-SummaryTable -FileExtension $File.Extension -Modified $False
-
-        if ($Verbose.IsPresent) {
-            Write-Verbose ("VERBOSE: Ignoring unrecognized target: " + $_.FullName)
-        }
-        if ($WhatIf.IsPresent) {
-            Write-Output -InputObject ("What if: Would ignore modifying on unrecognized target: " + $_.FullName)
-        }
-    }
-}
-
 function Set-SummaryTable {
     Param
     (
         [Parameter(Mandatory = $True)]
-        [string]$FileExtension,
+        [string]$FileItem,
 
         [Parameter(Mandatory = $True)]
         [bool]$Modified
     )
     # TODO: perhaps Group-Object can be used in here
-    if ($SummaryTable.ContainsKey($FileExtension) -eq $True) {
-        ($SummaryTable[$FileExtension].Count)++
+    if ($SummaryTable.ContainsKey($FileItem) -eq $True) {
+        ($SummaryTable[$FileItem].Count)++
     }
     else {
         $YesNo = if ($Modified -eq $True) {'Yes'} else {'No'}
         $NewEntry = [PSCustomObject]@{Count = 1; Modified = $YesNo}
-        $SummaryTable.Add($FileExtension, $NewEntry)
+        $SummaryTable.Add($FileItem, $NewEntry)
     }
 }
 
@@ -409,13 +344,13 @@ function Get-FileTypeBrackets {
 
     process {
     
-        $KeyFound = $FileTypeTable.GetEnumerator() | Where-Object -Property Value  -Match $FileExtension | Select-Object -ExpandProperty Name
+        $KeyFound = $FileTypeTable.GetEnumerator() | Where-Object -Property Value -Match $FileExtension | Select-Object -ExpandProperty Name
 
         if ($KeyFound) {
             $BracketsRaw = $BracketTable[$KeyFound]
             $Brackets = [PSCustomObject]@{
-                Opening = $BracketsRaw.Trim().Split(',')[0]
-                Closing = $BracketsRaw.Trim().Split(',')[1]
+                Opening = $BracketsRaw.Split(',')[0].Trim()
+                Closing = $BracketsRaw.Split(',')[1].Trim()
             }
         }
         else {
@@ -427,74 +362,209 @@ function Get-FileTypeBrackets {
     }
 }
 
-function Write-ContentsToFile {
-    [CmdletBinding()]
+<#
+.SYNOPSIS
+Set variables for Write-File which is next in the pipeline
 
+.DESCRIPTION
+Opens StreamReader and BinaryReader to set variables for Write-File which is next in the pipeline.  This will
+close StreamReader and BinaryReader after all variables are set
+
+.PARAMETER Path
+Path to the file.
+
+.EXAMPLE
+An example
+
+.NOTES
+System.IO.BinaryReader is being used here to retrieve the line endings as I believe this is the only way it can be done.
+#>
+function Open-File {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $True)]
+        [string]$FilePath
+    )
+
+    $FileItem = Get-Item -Path $FilePath
+    New-Object -TypeName System.IO.StreamReader -ArgumentList $FileItem.FullName -OutVariable StreamReader | Out-Null
+    # See NOTES for the function on why this is being used.
+    New-Object -TypeName System.IO.BinaryReader -ArgumentList $StreamReader.BaseStream -OutVariable BinaryReader | Out-Null
+
+    $FileAsString = $StreamReader.ReadToEnd();
+    
     [byte]$CR = 0x0D # 13  or  \r\n  or  `r`n
     [byte]$LF = 0x0A # 10  or  \n    or  `n
-
-    New-Object -TypeName System.IO.StreamReader -ArgumentList $Path -OutVariable StreamReader | Out-Null
-
-    switch ($StreamReader.CurrentEncoding.WebName.Replace('-', '')) {
-        'ascii' { $Encoding = New-Object -TypeName System.Text.ASCIIEncoding -ArgumentList $false; break}
-        'utf7' { $Encoding = New-Object -TypeName System.Text.UTF7Encoding -ArgumentList $false; break}
-        'utf8' { $Encoding = New-Object -TypeName System.Text.UTF8Encoding -ArgumentList $false; break}
-        'utf32' { $Encoding = New-Object -TypeName System.Text.UTF32Encoding -ArgumentList $false; break}
-        default { 
-            # for Out-File the default value is 'Unicode', so follow the same suit
-            $Encoding = New-Object -TypeName System.Text.UnicodeEncoding -ArgumentList $false;
-        }
-    }
-
-    New-Object -TypeName System.IO.BinaryReader -ArgumentList $StreamReader.BaseStream -OutVariable BinaryReader | Out-Null
-    
-    # this if statement is to determine the file's line endings and change the text that 
-    # will be written to the file, if needed.
-    $EOL = ''
     if ($BinaryReader.BaseStream.CanRead -eq $true -and $BinaryReader.BaseStream.Length -gt 0) {
         $BinaryReader.BaseStream.Position = 0
-        $BytesRead = $BinaryReader.ReadBytes($BinaryReader.BaseStream.Length)
-        $IndexOfLF = $BytesRead.IndexOf($LF)
-        $RemoveLastLine = ($BytesRead.Get($BytesRead.Length - 1) -eq $LF)
-        
-        if ($IndexOfLF) {
-            if ($RemoveLastLine) {
-                $HeaderPrependedToValue = $HeaderPrependedToValue.TrimEnd("`r`n")
-            }
-            # check previous char for 'CR', if so this file has 'CRLF' for EOL
-            # and we shouldnt have to do anything since PowerShell defaults to 
-            # the same EOL markings.  but if this file has lone LF endings, edit
-            # $HeaderPrependedToValue to have just LF endings too. Although the 
-            # following global PS variables may be benefical here, I'm considering
-            # individual files EOL markings.
-            # $OutputEncoding
-            # $OFS = $Info.LineEnding
-            if ($BytesRead[$IndexOfLF - 1] -ne $CR) {
-                $EOL = 'LF'
+        $FileAsBytes = $BinaryReader.ReadBytes($BinaryReader.BaseStream.Length)
+        $IndexOfLF = $FileAsBytes.IndexOf($LF)
+        $RemoveLastLine = ($FileAsBytes.Get($FileAsBytes.Length - 1) -eq $LF)
 
-                $HeaderPrependedToValue = $HeaderPrependedToValue -replace "`r", ""
-            }
-            else {
-                $EOL = 'CRLF'
-            }
-        }
-    }
-
-    $StreamReader.Close()
-    $BinaryReader.Close()
-    # $OFS = ""
-    if (!$WhatIf.IsPresent) {
-        [System.IO.File]::WriteAllLines($Path, $HeaderPrependedToValue, $Encoding)
-    }
-    else {
-        if ($EOL) {
-            Write-Output -InputObject ("What if: For file $Path, 
-will be encoded as '" + $Encoding.WebName + "' with end-of-line markings of '" + $EOL + "'")
+        $IndexOfLF = $FileAsBytes.IndexOf($LF)
+        if ($FileAsBytes[$IndexOfLF - 1] -ne $CR) {
+            $EOL = 'LF'
         }
         else {
-            Write-Output -InputObject ("What if: For file $Path, 
-will be encoded as '" + $Encoding.WebName)
+            $EOL = 'CRLF'
         }
+    }
+    
+    
+
+    # this if statement is to determine the file's line endings and change the text that 
+    # will be written to the file, if needed.
+
+    $BinaryReader.Dispose()
+    $StreamReader.Dispose()
+
+    $Data = [PsObject]@{
+        RemoveLastLine = $RemoveLastLine
+        FileItem       = $FileItem
+        EOL            = $EOL
+        FileAsString   = $FileAsString
+        Encoding       = ''
+    }
+    $Data 
+}
+
+<#
+.SYNOPSIS
+From the variables set from Open-File, Write-File will make logically decisions on what and how the contents 
+are arranged and written to file.
+
+.DESCRIPTION
+Long description
+
+.PARAMETER Header
+User defined header
+
+.PARAMETER Include
+Inclusion filter for files
+
+.PARAMETER WhatIf
+If true running in simulation mode
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
+function Write-File {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(ValueFromPipeline = $True)]
+        [PsObject]$Data,
+
+        [Parameter(Mandatory = $True)]
+        [string]$Header,
+
+        [Parameter(Mandatory = $False)]
+        [AllowEmptyString()]
+        [AllowNull()]
+        [string]$Include,
+
+        [Parameter(Mandatory = $True)]
+        [switch]$WhatIf
+    )
+
+    New-Object -TypeName System.IO.StreamWriter -ArgumentList $Data.FileItem.FullName -OutVariable StreamWriter | Out-Null
+
+    if ($Data.FileItem.Extension -eq ".html") {
+        $HTMLDirective = $Data.FileAsString | Select-String -Pattern '.*DOCTYPE.*'
+        if ($HTMLDirective.Matches.Success) {
+            $Data.FileAsString = $Data.FileAsString.Replace($HTMLDirective.Matches.Value, "").Trim()
+        }
+    }
+
+    # if Include is defined and it contains the current Extension...
+    if ($Include -and $Include.Split(',').Contains('*' + $Data.FileItem.Extension) -eq $True) {
+        $HeaderPrependedToFileString = @"
+${Header}
+$($Data.FileAsString)
+"@
+    }
+    elseif (!$Include) {
+        $Brackets = Get-FileTypeBrackets -FileExtension $Data.FileItem.Extension
+
+        if ($Brackets) {
+            $HeaderPrependedToFileString = @"
+$($Brackets.Opening)
+${Header}
+$($Brackets.Closing)
+$($Data.FileAsString)
+"@
+        }
+    }
+
+    if ($HeaderPrependedToFileString) {
+        #if ($Data.RemoveLastLine) {
+        #   $HeaderPrependedToFileString = $HeaderPrependedToFileString.TrimEnd("`r`n")
+        #}
+
+        # add the DTD tag at the very top of file, if there was one...
+        if ($HTMLDirective.Matches.Success) {
+            $HeaderPrependedToFileString = $HeaderPrependedToFileString.Insert(0, $HTMLDirective.Matches.Value + "`r`n")
+        }
+    
+        # check previous char for 'CR', if so this file has 'CRLF' for EOL
+        # and we shouldnt have to do anything since PowerShell defaults to 
+        # the same EOL markings.  but if this file has lone LF endings, edit
+        # $HeaderPrependedToFileString to have just LF endings too. Although the 
+        # following global PS variables may be benefical here, I'm considering
+        # individual files EOL markings.
+        # $OutputEncoding
+        # $OFS = $Info.LineEnding
+        if ($Data.EOL -eq 'LF') {
+            $HeaderPrependedToFileString = $HeaderPrependedToFileString -replace "`r", ""
+        }
+
+        if (!$WhatIf.IsPresent) {
+            $StreamWriter.WriteLine($HeaderPrependedToFileString)
+        } 
+
+        Set-SummaryTable -FileItem $Data.FileItem.FullName -Modified $True
+    }
+    else {
+        Set-SummaryTable -FileItem $Data.FileItem.FullName -Modified $False
+    }
+
+    try {
+        $StreamWriter.Dispose()
+    }
+    catch {
+        Write-Host "!!!BinaryWriter or StreamWriter error!!!"
+    }
+    
+    $Data.Encoding = $StreamWriter.Encoding
+    $Data
+}
+
+function Close-File {
+    [CmdletBinding()]
+    Param 
+    (
+        [Parameter(ValueFromPipeline = $True)]
+        [PsObject]$Data
+    )
+
+    if ($Data.EOL) {
+        Write-Output -InputObject ("What if: For file " + $Data.FileItem.FullName + ", 
+will be encoded as '" + $Data.Encoding.WebName + "' with end-of-line markings of '" + $Data.EOL + "'")
+    }
+    else {
+        Write-Output -InputObject ("What if: For file " + $Data.FileItem.FullName + ", 
+will be encoded as '" + $Data.Encoding.WebName)
+    }
+
+    if ($Verbose.IsPresent) {
+        Write-Verbose ("VERBOSE: Ignoring unrecognized target: " + $Data.FileItem.FullName)
+    }
+    if ($WhatIf.IsPresent) {
+        Write-Output -InputObject ("What if: Would ignore modifying on unrecognized target: " + $Data.FileItem.FullName)
     }
 }
 
--- README.md
+++ README.md
@@ -1,80 +1,91 @@
+# PrependLicense
+
+PrependLicense is a PowerShell module that automates prepending license headers to code files.
+
 [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/marckassay/PrependLicense/blob/master/LICENSE) [![PS Gallery](https://img.shields.io/badge/install-PS%20Gallery-blue.svg)](https://www.powershellgallery.com/packages/PrependLicense/)
 
-# PrependLicense
-PrependLicense is a PowerShell module that automates prepending license headers to code files. 
+## Features
 
-### Features
 * Predefined GPL, MIT license functions via `Add-GPLHeader` and `Add-MITHeader`
 * Prepending custom header is available via `Add-Header` function which can also be used for unknown file types
 * Simulate what will happen via `WhatIf` switch on any of the "Add" functions as listed below
 * Sensitive to DTD tags (`<!DOCTYPE>`) in HTML files
 * Attempts to preserve existing file encoding and to preserve end-of-line (EOL) markings
 
-### Caveat
+## Caveat
+
 * In order to add license headers to files, this module needs to know the opening and closing comment brackets for each file type by extension.  This is predefined in the `PrependLicenseVariables.ps1` file.  Most likely you will need to modify this file for your needs or pass-in an inclusion string to `Add-Header` (read next paragraph).  When installed, you can find where this module resides on the filesystem executing:
-```
+
+```powershell
 Get-Module PrependLicense | Select-Object {$_.Path}
 ```
-  
+
    You can use the `Add-Header` to pass-in your own header with or without comment brackets included.  With the comment brackets included in the header, you can only target one type of file (I'm using the word "type" as a file having the same comment brackets as others).  See `Add-Header` function in the Usage section for more information.
 
-* This module was developed for another repo of mine, which you can see the [commit](https://github.com/marckassay/AIT/commit/8505bfbf50137fc4ef238f311f818c4ab7a0354b) I made using PrependLicense.  As of this typing, PrependLicense module worked for my needs in an environment of: 
-   - Windows 10
-   - PowerShell 5.1.16299.98
-   - UTF-8, primarily EOL of 'LF'
+* This module was developed for another repo of mine, which you can see the [commit](https://github.com/marckassay/AIT/commit/8505bfbf50137fc4ef238f311f818c4ab7a0354b) I made using PrependLicense.  As of this typing, PrependLicense module worked for my needs in an environment of:
+  * Windows 10
+  * PowerShell 5.1.16299.98
+  * UTF-8, primarily EOL of 'LF'
 
 * There seems to be a change of last EOL marking on files.  A ticket is opened for this [issue](https://github.com/marckassay/PrependLicense/issues/1).
 
-
-
 ## Instructions
+
 * To install with PowerShellGet run the following command below.  Or download project to your PowerShell Module directory.
     ```powershell
     Install-Module PrependLicense
     ```
 
 ## Usage
+
 * All 3 methods below have the option of running in simulation mode, via `WhatIf` switch.  I recommend to initially switch `WhatIf` to verify what will and will not be modified.
 
-   ### Add-GPLHeader
-   ```powershell
-   $ Add-GPLHeader -Path .\src\ -ProgramName 'AiT' -ProgramDescription 'Another Interval Timer' -Author 'Marc Kassay'
-   ```
+### Add-GPLHeader
 
-   Using the `WhatIf` switch.
-   ```powershell
-   $ Add-GPLHeader -Path .\src\ -ProgramName 'AiT' -ProgramDescription 'Another Interval Timer' -Author 'Marc Kassay' -WhatIf
-   ```
+```powershell
+$ Add-GPLHeader -Path .\src\ -ProgramName 'AiT' -ProgramDescription 'Another Interval Timer' -Author 'Marc Kassay'
+```
 
-   ### Add-MITHeader
-   Notice this function doesn't take the `ProgramDescription` parameter as the `Add-GPLHeader` requires.
-   ```powershell
-   $ Add-MITHeader -Path .\src\ -ProgramName 'AiT' -Author 'Marc Kassay'
-   ```
+Using the `WhatIf` switch.
 
-   ### Add-Header
-   When using the generic `Add-Header` function, I recommend to use "here-string" as shown below.  For information on "here-string" visit this [about link](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_quoting_rules?view=powershell-5.1) and goto the section titled "HERE-STRINGS".
+```powershell
+$ Add-GPLHeader -Path .\src\ -ProgramName 'AiT' -ProgramDescription 'Another Interval Timer' -Author 'Marc Kassay' -WhatIf
+```
 
-   Below, `$MarcsLicense` is a header variable that is used with `Add-Header`.  The value passed to `Header` parameter will be applied to all predefined file types that are in the `PrependLicenseVariables.ps1` file, unless the `Include` parameter is used.
-  
-   The `Include` parameter can be used to target files types that are not predefined.  If this is used, then the value for `Header` parameter must be in a form of a comment.  In otherwords, if you want to prepend file types unknown to this module, you must include the header in a form of a comment.
+### Add-MITHeader
 
-   To see a session using `Add-Header` for known and unknown file types see the file in this repo: example/example-1.txt
+Notice this function doesn't take the `ProgramDescription` parameter as the `Add-GPLHeader` requires.
 
-   Using `Add-Header` to apply a custom header with predefined file types that are listed in the `PrependLicenseVariables.ps1` file.
-   ```powershell
-   $ $MarcsLicense = @"
-       AS OF: JAN2018:
-       DO NOT: 'SELL', 'TRADE' or 'EXCHANGE' CODE BELOW!!!
-   "@
-   $ Add-Header -Path .\src\ -Header $MarcsLicense
-   ```
+```powershell
+$ Add-MITHeader -Path .\src\ -ProgramName 'AiT' -Author 'Marc Kassay'
+```
 
-   Using `Add-Header` to apply a custom header in a form of a comment to unknown file types that are set in the `Include` parameter.
-   ```powershell
-   $ $MarcsLicense = @"
-       %% AS OF: JAN2018:
-       %% DO NOT: 'SELL', 'TRADE' or 'EXCHANGE' CODE BELOW!!!
-   "@
-   $ Add-Header -Path .\src\ -Header $MarcsLicense -Include '*.m52, *.m53'
-   ```
+### Add-Header
+
+When using the generic `Add-Header` function, I recommend to use "here-string" as shown below.  For information on "here-string" visit this [about link](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_quoting_rules?view=powershell-5.1) and goto the section titled "HERE-STRINGS".
+
+Below, `$MarcsLicense` is a header variable that is used with `Add-Header`.  The value passed to `Header` parameter will be applied to all predefined file types that are in the `PrependLicenseVariables.ps1` file, unless the `Include` parameter is used.
+
+The `Include` parameter can be used to target files types that are not predefined.  If this is used, then the value for `Header` parameter must be in a form of a comment.  In otherwords, if you want to prepend file types unknown to this module, you must include the header in a form of a comment.
+
+To see a session using `Add-Header` for known and unknown file types see the file in this repo: example/example-1.txt
+
+Using `Add-Header` to apply a custom header with predefined file types that are listed in the `PrependLicenseVariables.ps1` file.
+
+```powershell
+$ $MarcsLicense = @"
+    AS OF: JAN2018:
+    DO NOT: 'SELL', 'TRADE' or 'EXCHANGE' CODE BELOW!!!
+"@
+$ Add-Header -Path .\src\ -Header $MarcsLicense
+```
+
+Using `Add-Header` to apply a custom header in a form of a comment to unknown file types that are set in the `Include` parameter.
+
+```powershell
+$ $MarcsLicense = @"
+    %% AS OF: JAN2018:
+    %% DO NOT: 'SELL', 'TRADE' or 'EXCHANGE' CODE BELOW!!!
+"@
+$ Add-Header -Path .\src\ -Header $MarcsLicense -Include '*.m52, *.m53'
+```
